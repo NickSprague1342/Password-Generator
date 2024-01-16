b9c9fbdf22ff56e50076576c2770ddcf772be3a8
Nick Sprague <n.sprague18@gmail.com>
2024-01-16 11:48:09 -0500

    added script.js prompts for confirm characters, and added function for password textbox

--- script.js
+++ script.js
@@ -19,11 +19,32 @@ function generatePassword() {
   enter = parseInt(prompt('How many characters would you like your password? Choose between 8 and 128 characters.'));
 }
 
+//if user does not select any criteria throw error and have user choose from criteria again.
 if (!enter) {
   alert('This needs a value in order to proceed!');
 } else if (enter < 8 || enter > 128) {
-  
+    enter = parseInt(prompt('You must choose between 8 and 128'));
+} else {
+  upperCaseArr = confirm('Would you like you password to contain Uppercase Letters?');
+  lowerCaseArr = confirm('Would you like your password to contain Lowercase Letters?');
+  specialCharacter = confirm('Would you like your password to contain any special characters?');
+  number = confirm('Would you like your password to contain numbers?');
+};
+
+if (!upperCaseArr && !lowerCaseArr && !specialCharacter && !number) {
+  choice = alert('You must enter at least one criteria');
 }
+
+
+//puts password in provided textbox
+function UserInput(newps) {
+  document.getElementById('password').textContent = newps;
+}
+
+var copy = document.querySelector('#copy');
+copy.addEventListener('click', function(){
+  copyPassword();
+});
 
 
 
