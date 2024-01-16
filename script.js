00b56ce776ee5155c23d3d79356bd5c27b59aad3
Nick Sprague <n.sprague18@gmail.com>
2024-01-15 19:54:54 -0500

    added variables and event listener to script.js

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
+//input variables for uppercase, lower case, special character, and number
 var upperCaseArr = Array.from('QWERTYUIOPASDFGHJKLZXCVBNM')
 var lowerCaseArr = Array.from('qwertyuiopasdfghjklzxcvbnm')
 var specialCharacter = Array.from('`~!@#$%^&*()-_=+\|]}[{";:/?.>,<')
+var number = Array.from('0123456789')
 
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var get = document.querySelector('#generte');
 
-  passwordText.value = password;
+get.addEventListener('click', function () {
+  newps = generatePassword();
+  document.getElementById('password').placeholder = newps
+});
 
+function generatePassword() {
+  enter = parseInt(prompt('How many characters would you like your password? Choose between 8 and 128 characters.'));
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+if (!enter) {
+  alert('This needs a value in order to proceed!');
+} else if (enter < 8 || enter > 128) {
+  
+}
+
+
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
 
 
