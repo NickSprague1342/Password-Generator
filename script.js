1ce2c0f883c21fe9224a9d2b0ed50513c02cb5b9
Nick Sprague <n.sprague18@gmail.com>
2024-01-19 21:40:31 -0500

    added if and else if statements, still working on getting password to generate

--- script.js
+++ script.js
@@ -1,69 +1,131 @@
+// User input variables: 
+var enter;
+var confirmUppercase;
+var confirmLowercase;
+var confirmSpecialCharacter;
+var confirmNumber;
+
 //input variables for uppercase, lower case, special character, and number
 var upperCaseArr = Array.from('QWERTYUIOPASDFGHJKLZXCVBNM')
 var lowerCaseArr = Array.from('qwertyuiopasdfghjklzxcvbnm')
 var specialCharacter = Array.from('`~!@#$%^&*()-_=+\|]}[{";:/?.>,<')
-var number = Array.from('0123456789')
+var numberArr = Array.from('0123456789')
 
+var choices;
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+var get = document.querySelector("#generate");
 
-var get = document.querySelector('#generte');
-
-get.addEventListener('click', function () {
-  newps = generatePassword();
-  document.getElementById('password').placeholder = newps
+get.addEventListener("click", function () {
+    newps = generatePassword();
+    document.getElementById("password").placeholder = newps;
 });
 
-
-//below function generates password on click
+//this is the generate password function
 function generatePassword() {
-  enter = parseInt(prompt('How many characters would you like your password? Choose between 8 and 128 characters.'));
+    // Asks user input
+    enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
+  
+    if (!enter) {
+        alert("This needs a value in order to proceed");
+    } else if (enter < 8 || enter > 128) {
+      
+        enter = parseInt(prompt("You must choose between 8 and 128"));
+       
+
+    } else {
+        // Continues once user input is validated 1-4
+        numberArr = confirm("Would you like your password to contain numbers?");
+        specialCharacter = confirm("Would you like your password to contain special characters?");
+        upperCaseArr = confirm("Would you like your password to contain Uppercase letters?");
+        lowerCaseArr = confirm("Would you like your password to contain Lowercase letters?");
+    };
+
+    // Else if for 4 negative options
+    if (!specialCharacter && !number && !upperCaseArr && !lowerCaseArr) {
+        choices = alert("You must choose at least one criteria!");
+
+
+
+
+        
+
+    }
+    // First if statement that uses user input prompts to determine choices
+    // Else if for 4 positive options
+    else if (specialCharacter && numberArr && upperCaseArr && lowerCaseArr) {
+
+        choices = specialCharacter.concat(numberArr, upperCaseArr, lowerCaseArr);
+    }
+    // Else if for 3 positive options
+    else if (specialCharacter && numberArr && upperCaseArr) {
+        choices = specialCharacter.concat(numberArr, upperCaseArr);
+    }
+    else if (specialCharacter && numberArr && lowerCaseArr) {
+        choices = specialCharacter.concat(numberArr, lowerCaseArr);
+    }
+    else if (specialCharacter && confirmLowercase && upperCaseArr) {
+        choices = specialCharacter.concat(lowerCaseArr, upperCaseArr);
+    }
+    else if (numberArr && lowerCaseArr && confirmUppercase) {
+        choices = numberArr.concat(lowerCaseArr, upperCaseArr);
+    }
+    
+    else if (specialCharacter && numberArr) {
+        choices = specialCharacter.concat(numberArr);
+
+    } else if (specialCharacter && lowerCaseArr) {
+        choices = specialCharacter.concat(lowerCaseArr);
+
+    } else if (specialCharacter && upperCaseArr) {
+        choices = specialCharacter.concat(upperCaseArr);
+    }
+    else if (lowerCaseArr && numberArr) {
+        choices = lowerCaseArr.concat(numberArr);
+
+    } else if (lowerCaseArr && upperCaseArr) {
+        choices = lowerCaseArr.concat(upperCaseArr);
+
+    } else if (numberArr && upperCaseArr) {
+        choices = numberArr.concat(upperCaseArr);
+    }
+   
+    else if (specialCharacter) {
+        choices = specialCharacter;
+    }
+    else if (numberArr) {
+        choices = numberArr;
+    }
+    else if (lowerCaseArr) {
+        choices = lowerCaseArr;
+    }
+   
+    else if (upperCaseArr) {
+        choices = upperCaseArr;
+    };
+
+    var password = [];
+
+   
+    // Random selection of all variables: 
+    for (var i = 0; i < enter; i++) {
+        var pickChoices = choices[Math.floor(Math.random() * choices.length)];
+        password.push(pickChoices);
+    }
+  
+    var newps = password.join("");
+    UserInput(newps);
+    return newps;
 }
+// password into the textbox
 
-//if user does not select any criteria throw error and have user choose from criteria again.
-if (!enter) {
-  alert('This needs a value in order to proceed!');
-} else if (enter < 8 || enter > 128) {
-    enter = parseInt(prompt('You must choose between 8 and 128'));
-} else {
-  upperCaseArr = confirm('Would you like you password to contain Uppercase Letters?');
-  lowerCaseArr = confirm('Would you like your password to contain Lowercase Letters?');
-  specialCharacter = confirm('Would you like your password to contain any special characters?');
-  number = confirm('Would you like your password to contain numbers?');
-};
-
-if (!upperCaseArr && !lowerCaseArr && !specialCharacter && !number) {
-  choice = alert('You must enter at least one criteria');
-}
-
-
-
-//puts password in provided textbox
 function UserInput(newps) {
-  document.getElementById('password').textContent = newps;
+    document.getElementById("password").textContent = newps;
+
 }
 
-var copy = document.querySelector('#copy');
-copy.addEventListener('click', function(){
-  copyPassword();
+var copy = document.querySelector("#copy");
+copy.addEventListener("click", function () {
+    copyPassword();
 });
-
-
-
-
-
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
 
 
